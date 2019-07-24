83c7d25b539b0d3ae86ce37c3d326e4462db52ba
southern-yan <468137075@qq.com>
2019-07-24 22:05:20 +0800

    搭建一个简单的http服务器

---
+++ HTTP/http_server.cc
@@ -0,0 +1,21 @@
+#include "hello_http_server.hpp"
+
+void Usage(string proc)
+{
+    cout << "Usage£º" << proc << "ip port" << endl;
+}
+
+int main(int argc, char*  argv[])
+{
+    if(argc != 3)
+    {
+        Usage(argv[0]);
+        exit(1);
+    }
+    int port = atoi(argv[2]);
+    string ip = argv[1];
+    TcpServer *tsp = new TcpServer(ip, port);
+    tsp->InitServer();
+    tsp->Start();
+    return 0;
+}
